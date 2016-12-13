8ea16a570f244c83eebfa824f6d5ed279a4eaff1
dannylui91 <dannylui91@gmail.com>
2016-12-12 20:29:05 -0500

    Added Notification part

--- app/src/main/java/nyc/c4q/jonathancolon/catchemall/MyTestService.java
+++ app/src/main/java/nyc/c4q/jonathancolon/catchemall/MyTestService.java
@@ -1,7 +1,12 @@
 package nyc.c4q.jonathancolon.catchemall;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 import java.util.Random;
@@ -24,5 +29,34 @@ public class MyTestService extends IntentService {
         Random rand = new Random();
         int randNum = rand.nextInt(20);
         System.out.println("Random Number: " + randNum);
+
+        //showNotification();
+    }
+
+    public void showNotification() {
+        int NOTIFICATION_ID = 555;
+
+        // Define an intent to trigger when notification is selected (in this case to open an activity)
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        // Turn this into a PendingIntent
+        int requestID = (int) System.currentTimeMillis(); // Unique requestID to differentiate between various notification with same notification ID
+        int flags = PendingIntent.FLAG_CANCEL_CURRENT; // Cancel old intent and create new one
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, requestID, intent, flags);
+
+        // Attach the pendingIntent to a new notification using setContentIntent
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true) // Hides the notification after its been selected
+                .build();
+
+        // Get the notification manager system service
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Setting a notification ID allows you to update the notification later on.
+        notificationManager.notify(NOTIFICATION_ID, notification);
     }
 }
